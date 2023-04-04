d4e322c2e43d67aeae0f4c0cc30a186f7b3fb277
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-04-04 16:13:08 +0530

    printReport finished

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require("./crawl.js");
+const { printReport } = require("./report.js");
 
 async function main() {
   if (process.argv.length < 3) {
@@ -11,17 +12,12 @@ async function main() {
     process.exit(1);
   }
 
-  //   for (const arg of process.argv) {
-  //     console.log(arg);
-  //   }
   const baseURL = process.argv[2];
 
   console.log(`starting crawl of ${baseURL}`);
   const pages = await crawlPage(baseURL, baseURL, {});
 
-  for (const page of Object.entries(pages)) {
-    console.log(page);
-  }
+  printReport(pages);
 }
 
 main();
